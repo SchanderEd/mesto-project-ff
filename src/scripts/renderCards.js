b6eb4e2e571fb5179c0e7dd382b2f01dcad541fc
Eduard <dumpweed10@gmail.com>
2024-05-28 18:17:44 +0300

    added webpack

--- src/scripts/renderCards.js
+++ src/scripts/renderCards.js
@@ -1,13 +1,3 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
 import { initialCards } from "./cards.js";
 
 const cardsList = document.querySelector('.places__list');
